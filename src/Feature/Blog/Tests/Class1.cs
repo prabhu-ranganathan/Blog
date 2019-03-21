6e222216bb4b3fc41bfaee084fe5fc48fb00962d
Prabhu Ranganathan <Prabhu.Ranganathan@marlabs.com>
2019-03-21 17:57:41 +0530

    Add project files.

---
+++ src/Feature/Blog/Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Claro.Feature.Blog.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Foundation/Core/code/Exceptions/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Claro.Foundation.Core.Exceptions
+{
+    public static class Constants
+    {
+        public static class InvalidMediatorResponse
+        {
+            public static string InvalidCodeReturned = "Invalid Mediator Code Returned";
+        }
+    }
+}
